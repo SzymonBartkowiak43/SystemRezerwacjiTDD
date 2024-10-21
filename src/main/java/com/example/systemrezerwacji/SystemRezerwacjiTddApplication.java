409eeaa1825a9b3e87f9076cbadaa2d63febc503
Szymon Bartkowiak <szymon.b4310@gmail.com>
2024-10-21 10:32:39 +0200

    init

---
+++ src/main/java/com/example/systemrezerwacji/SystemRezerwacjiTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemrezerwacji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemRezerwacjiTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemRezerwacjiTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/systemrezerwacji/SystemRezerwacjiTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemrezerwacji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemRezerwacjiTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
